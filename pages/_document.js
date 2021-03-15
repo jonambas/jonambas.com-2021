20c1525f932f2ac3cf690b8c9cda6a0116c0a85e
Jon Ambas <jon@jonambas.com>
2021-03-14 20:15:20 -0400

    Add GA

--- pages/_document.js
+++ pages/_document.js
@@ -32,7 +32,17 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=UA-29511296-7"
+          ></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'UA-29511296-7');`,
+            }}
+          ></script>
+        </Head>
         <body>
           <Main />
           <NextScript />
